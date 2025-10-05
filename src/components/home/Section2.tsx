5a2a5498b735177ec37b0a143adda1837430514f
Niranjan Panigrahi <niranjanpanigrahi2016@gmail.com>
2025-10-06 00:47:37 +0530

    implemented new sections and layout design stuffs

--- src/components/home/Section2.tsx
+++ src/components/home/Section2.tsx
@@ -5,17 +5,17 @@ import { HoverLayoutGrid } from "@/components/common/HoverLayoutGrid";
 import services from "@/data/services";
 
 const HomeSection2 = ({ id }: { id: string }) => {
-  // return (
-    // <ResponsiveBox
-    //   classNames="dark:bg-[var(--dialogColor)] bg-[var(--dialogColor)] min-h-screen items-center justify-center dark:bg-dot-white/[0.15] bg-dot-white/[0.15] rounded-md"
-    //   id={id}
-    // >
-    //   <ConstrainedBox classNames="p-4 py-16 z-20">
-    //     <SectionTitle>Services</SectionTitle>
-    //     <HoverLayoutGrid cards={services} />
-    //   </ConstrainedBox>
-    // </ResponsiveBox>
-  // );
+  return (
+    <ResponsiveBox
+      classNames="dark:bg-[var(--dialogColor)] bg-[var(--dialogColor)] min-h-screen items-center justify-center dark:bg-dot-white/[0.15] bg-dot-white/[0.15] rounded-md"
+      id={id}
+    >
+      <ConstrainedBox classNames="p-4 py-16 z-20">
+        <SectionTitle>Services</SectionTitle>
+        <HoverLayoutGrid cards={services} />
+      </ConstrainedBox>
+    </ResponsiveBox>
+  );
 };
 
 export default HomeSection2;
