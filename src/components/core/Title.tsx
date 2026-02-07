ea541c5a127c8314267843ef832ed38ad1cb172a
Niranjan Panigrahi <niranjanpanigrahi2016@gmail.com>
2026-02-07 11:48:06 +0530

    implemented new sections and layout designs

--- src/components/core/Title.tsx
+++ src/components/core/Title.tsx
@@ -1,10 +1,14 @@
-const Title = ({ title }) => {
-    return (
-        <div>
-            <h1 className='text-4xl font-bold'>{title}</h1>
-            <div className='title-bar' />
-        </div>
-    )
-}
+type TitleProps = {
+  title: string;
+};
+
+const Title = ({ title }: TitleProps) => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <div className="title-bar" />
+    </div>
+  );
+};
 
 export default Title;
